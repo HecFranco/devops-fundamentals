bfcee3c9e8a229a5a273e1a4ffece11dbb06aec4
daniel.sanchez <daniel.sanchez@lemoncode.net>
2020-11-11 14:21:41 +0100

    implement example 06

--- 00-webpack/06-custom-css/webpack.config.js
+++ 00-webpack/06-custom-css/webpack.config.js
@@ -1,13 +1,28 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: ["./students.js"],
+  entry: {
+    app: "./students.js",
+    appStyles: ["./mystyles.css"],
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(process.cwd(), "dist"),
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
@@ -19,7 +34,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "index.html", //Name of file in ./dist/
       template: "index.html", //Name of template in ./src
-      hash: true,
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
   ],
 };
