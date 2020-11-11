26caa280da80bafdb6585fd3e4c2861cce6dab6c
daniel.sanchez <daniel.sanchez@lemoncode.net>
2020-11-11 13:55:39 +0100

    implement example 05

--- 00-webpack/05-output/webpack.config.js
+++ 00-webpack/05-output/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: ["./students.js"],
   module: {
@@ -12,4 +14,12 @@ module.exports = {
   devServer: {
     port: 8080,
   },
+  plugins: [
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: "index.html", //Name of file in ./dist/
+      template: "index.html", //Name of template in ./src
+      hash: true,
+    }),
+  ],
 };
